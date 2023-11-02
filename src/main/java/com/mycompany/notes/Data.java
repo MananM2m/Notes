119f3559954f4875c628473f1b9ba21f206ce61c
Manan Mishra <129023574+MananM2m@users.noreply.github.com>
2023-11-01 22:05:39 -0500

    Create Data.java

---
+++ src/main/java/com/mycompany/notes/Data.java
@@ -0,0 +1,14 @@
+package com.mycompany.notes;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author manan
+ */
+public class Data {
+    
+}
