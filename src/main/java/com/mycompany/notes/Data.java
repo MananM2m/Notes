1cc5684dbf562073590858467a6aca04096fef11
Manan Mishra <129023574+MananM2m@users.noreply.github.com>
2023-11-02 21:01:37 -0500

    Update Data.java

--- src/main/java/com/mycompany/notes/Data.java
+++ src/main/java/com/mycompany/notes/Data.java
@@ -1,5 +1,6 @@
 package com.mycompany.notes;
 
+
 import java.util.HashMap;
 import org.mindrot.jbcrypt.BCrypt;
 
@@ -9,23 +10,43 @@ import org.mindrot.jbcrypt.BCrypt;
  */
 public class Data implements java.io.Serializable{
     private Person loggedIn;
-    private HashMap<Integer, Person> data;
+    private HashMap<Integer , Person> people;
     private HashMap<Integer, String> users;
     
+    public Data(){
+        loggedIn = new Person("");
+        people = new HashMap<>();
+        users = new HashMap<>();
+    }
+    
     public boolean checkCombo(int username, String password){
-        
         if(BCrypt.checkpw(password, users.get(username)))
             return true;
-    
         return false;
     }
     
-    public Data(){
-        
-        loggedIn = new Person("");
-        
-        data = new HashMap<>();
-        users = new HashMap<>();
-        
+    public void newUser(String username, String password){
+        people.put(username.hashCode(), new Person(username));
+        users.put(username.hashCode(),BCrypt.hashpw(password, BCrypt.gensalt()));
+    }
+    
+    public Person getUser(int username){
+        return people.get(username);
+    }
+    
+    public void changePass(int username, String newPass){
+        users.put(username, newPass);
+    }
+    
+    public boolean userExists(int username){
+        return users.containsKey(username);
+    }
+    
+    public void setLoggedIn(int username){
+        loggedIn = people.get(username);
+    }
+    
+    public Person getLoggedIn(){
+        return loggedIn;
     }
 }
